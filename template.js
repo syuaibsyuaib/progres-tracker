eedea0dd3294a98401dd12ce09df0ed40a6a93b7
syuaibsyuaib <syuaibsyuaib@gmail.com>
2022-11-30 00:34:22 +0800

    sisa : isi template json kegiatan

---
+++ template.js
@@ -0,0 +1,47 @@
+let urlDb = "https://script.google.com/macros/s/AKfycbw-6436qjm4JSfLpfcC8KTQCIlkFaf5nDKnLaM_6OA9egn9ICfMXlqn9344RB1-euGe/exec"
+
+let db = []
+
+let templateDb = {
+    "id": "",
+    "nama proyek": "",
+    "panitia": [
+        // { "id": "", "nama": "" }
+    ],
+    "budget": {
+        "uraian": [
+            { "id": "", "nama barjas": "", "kuantitas": "", "satuan": "", "harga satuan": "", "total": "" },
+        ],
+        "total": ""
+    },
+    "kegiatan": {
+        "persiapan": [
+            {
+                "id": "",
+                "nama kegiatan": "",
+                "barjas": [{ "idf": "", "dibutuhkan": "", "terpakai": "" }],
+                "total barjas": "",
+                "status": ""
+            }
+        ],
+        "pelaksanaan": [
+            {
+                "id": "",
+                "nama kegiatan": "",
+                "barjas": [{ "idf": "", "dibutuhkan": "", "terpakai": "" }],
+                "total barjas": "",
+                "status": ""
+            }
+        ],
+        "pelaporan": [
+            {
+                "id": "",
+                "nama kegiatan": "",
+                "barjas": [{ "idf": "", "dibutuhkan": "", "terpakai": "" }],
+                "total barjas": "",
+                "status": ""
+            }
+        ]
+    },
+    "status": ""
+}
